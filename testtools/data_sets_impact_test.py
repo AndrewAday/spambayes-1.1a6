ca85d46c8adbe2a94bde1a755808463f54d179ac
Andrew Aday <andrewladay@gmail.com>
2015-10-25 04:08:14 -0400

    made generating dist_list parallel

--- testtools/data_sets_impact_test.py
+++ testtools/data_sets_impact_test.py
@@ -153,11 +153,12 @@ def noisy_data_check(pure_clusters, v_au):
 
 
 def main():
-    sets = [11,12,13,14,15] # select which data sets you want to run algorithm on
+    sets = [16] # select which data sets you want to run algorithm on
+    global dest
     if len(sys.argv) > 1:
         dest = sys.argv[1]
 
-    print "using custom destination: ", dest
+    print "path selected: ", dest
 
     for i in sets:
         ham = hams[i]
@@ -201,17 +202,17 @@ def main():
                                                       msgs.SpamStream(spam_p, [spam_p])],     # Training Spam
                                                      msgs.HamStream(ham_test, [ham_test]),          # Testing Ham
                                                      msgs.SpamStream(spam_test, [spam_test]),       # Testing Spam
-                                                     distance_opt="frequency4", all_opt=True,      
+                                                     distance_opt="frequency5", all_opt=True,      
                                                      update_opt="hybrid", greedy_opt=True,          
-                                                     include_unsures=False) # Don't unclude unsure emails        
+                                                     include_unsures=False, multi_process=False) # Don't unclude unsure emails        
 
             # vanilla active unlearner
-            v_au = ActiveUnlearnDriver.ActiveUnlearner([msgs.HamStream(ham_train, [ham_train]), []],
-                                                       [msgs.SpamStream(spam_train, [spam_train]), []],
-                                                       msgs.HamStream(ham_test, [ham_test]),
-                                                       msgs.SpamStream(spam_test, [spam_test]))
+            # v_au = ActiveUnlearnDriver.ActiveUnlearner([msgs.HamStream(ham_train, [ham_train]), []],
+            #                                            [msgs.SpamStream(spam_train, [spam_train]), []],
+            #                                            msgs.HamStream(ham_test, [ham_test]),
+            #                                            msgs.SpamStream(spam_test, [spam_test]))
 
-            vanilla_detection_rate = v_au.current_detection_rate
+            # vanilla_detection_rate = v_au.current_detection_rate
 
             time_2 = time.time()
             train_time = seconds_to_english(time_2 - time_1)
@@ -221,12 +222,12 @@ def main():
 
             with open(dest + data_set + " (unlearn_stats).txt", 'w+') as outfile:
                 try:
-                    unlearn_stats(au, outfile, data_set, [train_ham, train_spam], [test_ham, test_spam],
-                                  [ham_polluted, spam_polluted], total_polluted, total_unpolluted,
-                                  train_time, vanilla=[vanilla_detection_rate, v_au], noisy_clusters=True)
                     # unlearn_stats(au, outfile, data_set, [train_ham, train_spam], [test_ham, test_spam],
                     #               [ham_polluted, spam_polluted], total_polluted, total_unpolluted,
-                    #               train_time, vanilla=None, noisy_clusters=True)
+                    #               train_time, vanilla=[vanilla_detection_rate, v_au], noisy_clusters=True)
+                    unlearn_stats(au, outfile, data_set, [train_ham, train_spam], [test_ham, test_spam],
+                                  [ham_polluted, spam_polluted], total_polluted, total_unpolluted,
+                                  train_time, vanilla=None, noisy_clusters=True)
 
                 except KeyboardInterrupt:
                     outfile.flush()
