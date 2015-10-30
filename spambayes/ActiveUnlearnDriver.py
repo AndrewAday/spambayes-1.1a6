4a9e5096879362ff220d6307e3276e57aad8c99f
Andrew Aday <andrewladay@gmail.com>
2015-10-30 10:56:29 -0400

    debug

--- spambayes/ActiveUnlearnDriver.py
+++ spambayes/ActiveUnlearnDriver.py
@@ -56,6 +56,7 @@ def cluster_au_multi(au, gold=False, pos_cluster_opt=0, shrink_rejects=False, n_
     print "testing"
     print mislabeled[0].prob
     print ns.mislabeled_prob[0]
+    print ns.test.tag
     print dir(ns.test)
     for attr in dir(ns.test):
         try:
