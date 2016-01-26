671b8876e47c60e2e4d0eb4910a9601c2eab2b7e
andrewaday <andrewladay@gmail.com>
2016-01-25 20:02:18 -0500

    test results for partitioning

--- testtools/data_sets_impact_test.py
+++ testtools/data_sets_impact_test.py
@@ -160,8 +160,8 @@ def noisy_data_check(pure_clusters, v_au):
 
 
 def main():
-    sets = [11,12,13,14,15] # select which data sets you want to run algorithm on
-
+    # sets = [11,12,13,14,15] # select which data sets you want to run algorithm on
+    sets = [15]
     parser = argparse.ArgumentParser()
     parser.add_argument('-cv', '--cross', type=str, help="partition test set into T1 and T2 for cross-validation",
         choices=['random','features','mislabeled'], default=None)
